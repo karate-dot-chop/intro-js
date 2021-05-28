b96b6eaa241a8b73a21d12684cac6326c5f23dfe
dani <dani.zaghian@gmail.com>
2021-05-28 12:46:34 -0700

    adds day 2 problems

--- practice.js
+++ practice.js
@@ -135,4 +135,143 @@ function printSums(nums1, nums2) {
   });
 }
 
-printSums([1, 5, 10], [100, 500, 1000]);
+// printSums([1, 5, 10], [100, 500, 1000]);
+
+
+
+// DAY 2
+
+
+// 1. Write a function that takes in an array of numbers and returns its sum.
+function sum(numbers) {
+  var result = 0;
+  numbers.forEach(num => {
+    result += num;
+  });
+  return result;
+}
+// console.log(sum([1, 2, 3]));
+
+// 2. Write a function that takes in an array of strings and returns the smallest string.
+function smallestString(strings) {
+  var result = strings[0];
+  strings.forEach(string => {
+    if (string.length < result.length) {
+      result = string;
+    }
+  });
+  return result;
+}
+// console.log(smallestString(["apple", "grape", "grapes"]));
+
+// 3. Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+function reversedNumbers(numbers) {
+  var result = [];
+  var index = numbers.length - 1;
+  while (index >= 0) {
+    result.push(numbers[index]);
+    index--;
+  }
+  return result;
+}
+// console.log(reversedNumbers([1, 2, 3]));
+
+// 4. Write a function that takes in an array of words and returns the number of words that begin with the letter "a".
+function aWords(words) {
+  var result = 0;
+  words.forEach(word => {
+    if (word.startsWith("a")) {
+      result++;
+    }
+  });
+  return result;
+}
+// console.log(aWords(["apple", "orange", "grapes"]));
+
+// 5. Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+function joinWithCommas(strings) {
+  var result = "";
+  strings.forEach(string => {
+    result += `${string},`;
+  });
+  return result;
+}
+// console.log(joinWithCommas(["apple", "orange", "grapes"]));
+
+// 6. Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+function product(numbers) {
+  var result = 1;
+   numbers.forEach(number => {
+    result *= number;
+  });
+  return result;
+}
+
+// console.log(product([1, 2, 3, 4]));
+
+// 7. Write a function that takes in an array of numbers and returns the two smallest numbers.
+function twoSmallestNumbers(numbers) {
+  var smallestIndex = 0;
+  var smallest = numbers[smallestIndex];
+  var index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] < smallest) {
+      smallest = numbers[index];
+      smallestIndex = index;
+    }
+    index += 1;
+  }
+  var secondSmallest = numbers[smallestIndex + 1];
+  index = 0;
+  while (index < numbers.length) {
+    if (numbers[index] < secondSmallest && index !== smallestIndex) {
+      secondSmallest = numbers[index];
+    }
+    index += 1;
+  }
+  return [smallest, secondSmallest];
+}
+
+// console.log(twoSmallestNumbers([1, 10, 9, 3, 23, 12]));
+
+// 8. Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+function zeroCount(numbers) {
+  var result = 0;
+  numbers.forEach(number => {
+    if (number === 0) {
+      result++;
+    }
+  });
+  return result;
+}
+
+// console.log(zeroCount([9, 32, 0, 2, 0, 12, 5, 2, 0, 1]));
+
+// 9. Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+function isAllBig(numbers) {
+  var result = true;
+  numbers.forEach(number => {
+    if (number <= 10) {
+      result = false;
+    }
+  });
+  return result;
+}
+
+// console.log(isAllBig([11, 23, 32]));
+
+// 10. Write a function that takes in an array of words and returns the number of times the letter "a" appeared in total.
+function aCount(words) {
+  var result = 0;
+  words.forEach(word => {
+    word.split("").forEach(letter => {
+      if (letter === "a") {
+        result++;
+      }
+    });
+  });
+  return result;
+}
+
+console.log(aCount(["apple", "orange", "grapes"]));
+
--- practice.rb
+++ practice.rb
@@ -136,4 +136,146 @@ def print_sums(nums1, nums2)
   end
 end
 
-print_sums([1, 5, 10], [100, 500, 1000])
+# print_sums([1, 5, 10], [100, 500, 1000])
+
+
+# DAY 2
+
+
+# 1. Write a function that takes in an array of numbers and returns its sum.
+def sum(numbers)
+  result = 0
+  numbers.each do |num|
+    result += num
+  end
+  result
+end
+
+# p sum([1, 2, 3])
+
+# 2. Write a function that takes in an array of strings and returns the smallest string.
+def smallest_string(strings)
+  result = strings[0]
+  strings.each do |string|
+    if string.length < result.length
+      result = string
+    end
+  end
+  result
+end
+
+# p smallest_string(["apple", "orange", "grapes"])
+
+# 3. Write a function that takes in an array of numbers and returns a new array with the numbers in reverse order.
+def reversed_numbers(numbers)
+  result = []
+  index = numbers.length - 1
+  while index >= 0
+    result << numbers[index]
+    index -= 1
+  end
+  result
+end
+
+# p reversed_numbers([1, 2, 3])
+
+# 4. Write a function that takes in an array of words and returns the number of words that begin with the letter "a".
+def a_words(words)
+  result = 0
+  words.each do |word|
+    if word.starts_with?("a")
+      result += 1
+    end
+  end
+  result
+end
+
+# p a_words(["apple", "orange", "grapes"])
+
+# 5. Write a function that takes in an array of strings and joins them together to make a single string separated by commas.
+def join_with_commas(strings)
+  result = ""
+  strings.each do |string|
+    result += "#{string},"
+  end
+  result
+end
+
+p join_with_commas(["apple", "orange", "grapes"])
+
+# 6. Write a function that takes in an array of numbers and returns the product of all the numbers (each number multiplied by each other).
+def product(numbers)
+  result = 1
+  numbers.each do |number|
+    result *= number
+  end
+  result
+end
+
+# p product([1, 2, 3, 4])
+
+# 7. Write a function that takes in an array of numbers and returns the two smallest numbers.
+def two_smallest_numbers(numbers)
+  smallest_index = 0
+  smallest = numbers[smallest_index]
+  index = 0
+  while index < numbers.length
+    if numbers[index] < smallest
+      smallest = numbers[index]
+      smallest_index = index
+    end
+    index += 1
+  end
+  second_smallest = numbers[smallest_index - 1]
+  index = 0
+  while index < numbers.length
+    if numbers[index] < second_smallest && index != smallest_index
+      second_smallest = numbers[index]
+    end
+    index += 1
+  end
+  [smallest, second_smallest]
+end
+
+# p two_smallest_numbers([1, 10, 9, 3, 23, 12])
+
+# 8. Write a function that takes in an array of numbers and returns a count of how many zeros are in the array.
+def zero_count(numbers)
+  result = 0
+  numbers.each do |number|
+    if number == 0
+      result += 1
+    end
+  end
+  result
+end
+
+# p zero_count([9, 32, 0, 2, 0, 12, 5, 2, 0, 1])
+
+# 9. Write a function that takes in an array of numbers and returns true if all the numbers are bigger than 10, otherwise returns false.
+def is_all_big(numbers)
+  result = true
+  numbers.each do |number|
+    if number <= 10
+      result = false
+    end
+  end
+  result
+end
+
+# p is_all_big([11, 23, 32])
+
+# 10. Write a function that takes in an array of words and returns the number of times the letter "a" appeared in total.
+def a_count(words)
+  result = 0
+  words.each do |word|
+    word.each_char do |letter|
+      if letter == "a"
+        result += 1
+      end
+    end
+  end
+  result
+end
+
+p a_count(["apple", "orange", "grapes"])
