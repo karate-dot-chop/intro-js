bcf9a29317d5ed5eaa57fdb9098db76a66d75d0b
dani <dani.zaghian@gmail.com>
2021-05-28 14:03:56 -0700

    adds async callbacks

--- async.js
+++ async.js
@@ -1,4 +1,9 @@
 setTimeout(function() {
   console.log("First task complete!");
+  setTimeout(function() {
+    console.log("Second task complete!");
+    setTimeout(function() {
+      console.log("Third task complete!");
+    }, 2000);
+  }, 2000);
 }, 2000);
-console.log("Second task done!");
--- async.rb
+++ async.rb
@@ -1,3 +1,6 @@
 sleep 2
 puts "First task complete!"
+sleep 2
 puts "Second task complete!"
+sleep 2
+puts "Third task complete!"
